e2dcea80f2c92b858f6fb1cbfb7fc4bdaa976c70
jingta <jchow@where.com>
2011-03-04 22:21:10 -0500

    Initial rails app check in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monkeysee::Application.config.secret_token = '7c49dcc6d69a81413505f74f629326448df431620834172ea7976fbad19bdde12606a94b33382328754c4223270916da361c13b008dab1d36c5532579b4bdcd9'
