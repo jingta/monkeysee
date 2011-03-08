4df74813e6c0bb54da35bcfaf490798ee88df3fb
jingta <jchow@where.com>
2011-03-07 23:42:56 -0500

    Swapped out the service for a model

--- test/unit/github_post_receive_test.rb
+++ test/unit/github_post_receive_test.rb
@@ -1,8 +1,10 @@
 require 'test_helper'
 
 class GithubPostReceiveTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def test_parse_payload
+    payload = File.open('test/fixtures/files/payload.json', 'r').collect.join
+    GithubPostReceive.new JSON.parse(payload)
   end
+
 end
